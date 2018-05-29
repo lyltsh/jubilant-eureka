d4b9f92f53d258ccfac111308bbfb7ef150b1e4b
lyltsh@126.com <018764Aa>
2018-05-29 23:47:03 +0800

    art concurrent chapter01

---
+++ src/main/java/com/artConcurrent/code/chapter01/ConcurrencyTest.java
@@ -0,0 +1,55 @@
+package com.artConcurrent.code.chapter01;
+
+/**
+ * @Description: 并发和单线程执行测试
+ * @Author: leiyulin
+ * @date: 2018/5/29
+ */
+public class ConcurrencyTest {
+    /** 执行次数 */
+    private static final long count = 100000000000l;
+
+    public static void main(String[] args) throws InterruptedException {
+        //并发计算
+        concurrency();
+        //单线程计算
+        serial();
+    }
+
+    private static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+                System.out.println(a);
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        //在a，b两个循环结束之后，使用join方法，才计算并发执行的时间
+        thread.join();
+        long time = System.currentTimeMillis() - start;
+        System.out.println("concurrency :" + time + "ms,b=" + b);
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial:" + time + "ms,b=" + b + ",a=" + a);
+    }
+}
